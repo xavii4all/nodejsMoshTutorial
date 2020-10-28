7eee8689fa3e8a9df06a959f848984da3d2a584f
jpelliza <jpelliza.gcba@gmail.com>
2020-10-28 20:02:39 -0300

    Probando el debugger

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,5 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const config = require('config');
 const morgan = require('morgan');
 const express = require('express');
@@ -6,18 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(helmet());
-
-
-console.log('Application Name: ' + config.get('name'));
-console.log('Mail Server: ' + config.get('mail.host'));
-console.log('Mail Password: ' + config.get('mail.password'));
 
 
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    startupDebugger('Morgan enabled...');
 }
+
+// Db work
+dbDebugger('Connected to the database...');
 
 // PORT
 const port = process.env.PORT || 3000;
