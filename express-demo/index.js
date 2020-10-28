a16ed6691c0e9aef06849635cdeb1fc99ed06ac9
jpelliza <jpelliza.gcba@gmail.com>
2020-10-28 19:43:04 -0300

    First express little app

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,3 @@
-const config = require('config');
-const morgan = require('morgan');
-const helmet = require('helmet');
 const Joi = require('joi');
 const logger = require('./logger');
 const express = require('express');
@@ -9,21 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(helmet());
-
-
-console.log('Application Name: ' + config.get('name'));
-console.log('Mail Server: ' + config.get('mail.host'));
-console.log('Mail Password: ' + config.get('mail.password'));
-
-
-
-
-
-if (app.get('env') === 'development'){
-    app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
-}
 
 app.use(logger);
 
