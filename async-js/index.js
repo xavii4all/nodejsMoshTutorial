39210e67e0a6ba045b2710faefb01ce1c4add26e
jpelliza <jpelliza.gcba@gmail.com>
2020-11-02 13:35:33 -0300

    Named functions to prevent callback hell

---
+++ async-js/index.js
@@ -0,0 +1,28 @@
+console.log('Before');
+getUser(1, getRepositories);
+console.log('After');
+
+function getRepositories(user) {
+  getRepositories(user.gitHubUsername, getCommits);
+}
+function getCommits(repos){
+  getCommits(repo, displayCommits);
+}
+function displayCommits(commits){
+  console.log(commits);
+}
+
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log('Reading a user from a database...');
+    callback({ id: id, gitHubUsername: 'xavii' });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    callback(['repo1', 'repo2', 'repo3']);
+  }, 2000);
+}
