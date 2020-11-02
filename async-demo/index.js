f079d02efc259dc6c0b709c3a913a22f0e4f9a2d
jpelliza <jpelliza.gcba@gmail.com>
2020-11-02 17:26:13 -0300

    await async

--- async-demo/index.js
+++ async-demo/index.js
@@ -8,11 +8,25 @@ console.log('Before');
 // });
 console.log('After');
 
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log('Commits', commits))
-  .catch(err => console.log('Error', err.message));
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log('Commits', commits))
+//   .catch(err => console.log('Error', err.message));
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+  catch (err) {
+    console.log('Error', err.message);
+  }
+}
+
+displayCommits();
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
@@ -35,7 +49,7 @@ function getRepositories(username) {
 function getCommits(repo) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('Calling GitHub API...');
+      console.log('Getting commits...');
       resolve(['commit']);
     }, 2000);
   });
